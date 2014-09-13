f032e6e44cc550cf53c717e764a2972fb9189259
Nicolas Widart <n.widart@gmail.com>
2014-09-13 19:57:23 +0200

    Added some basic caching

--- src/Nwidart/Activity/ActivityServiceProvider.php
+++ src/Nwidart/Activity/ActivityServiceProvider.php
@@ -1,6 +1,8 @@
 <?php namespace Nwidart\Activity;
 
 use Github\Client;
+use Github\HttpClient\Cache\FilesystemCache;
+use Github\HttpClient\CachedHttpClient;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
@@ -30,9 +32,13 @@ class ActivityServiceProvider extends ServiceProvider
         $this->app['activity'] = $this->app->share(function($app) {
             $driver = $app['config']->get('activity::driver');
             $factoryClassName = "Nwidart\\Activity\\{$driver}\\{$driver}EventFactory";
-            $factory = new $factoryClassName;
 
-            $client = new Client;
+            $factory = new $factoryClassName;
+            $client = new CachedHttpClient();
+            $client->setCache(
+                new FilesystemCache(app_path() . '/storage/github-api-cache')
+            );
+            $client = new Client($client);
             $client->authenticate($app['config']->get('activity::github.token'), 'http_token');
 
             return new Activity($factory, $client);
